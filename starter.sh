33722fb99e85e8c16df36136bcc0055d97d76b22
Paras <git@eparas.com>
2019-07-12 12:40:38 -0500

    Test Commit

--- starter.sh
+++ starter.sh
@@ -1,8 +1,19 @@
-sudo nodemon app.js &
+#!/bin/bash
+
+sudo node app.js &
 app=$!
+sleep 3
 
-status=`git status -uno | sed -n '2p'`
-
-kill -STOP $app
-git pull
-kill -CONT $app 
+while [ 1 ]; do
+	status=`git status -uno | sed -n '2p' | cut -c 16-20`
+	if [ "$status" != "up to" ]; then
+		sudo kill -0 $app; 
+		git pull
+		sudo node app.js &
+		app=$!
+		sleep 3
+	else
+		echo "Up to date"
+		sleep 5
+	fi
+done

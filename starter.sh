84e994da5e8807d5c4337a9ecb5a5e462c4cc5e1
shparas <git@eparas.com>
2019-07-12 13:10:34 -0500

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -5,15 +5,18 @@ app=$!
 sleep 3
 
 while [ 1 ]; do
+	status=`git remote update`
 	status=`git status -uno | sed -n '2p' | cut -c 16-20`
 	if [ "$status" != "up to" ]; then
-		sudo kill -0 $app; 
-		git pull
+		sudo kill -0 $app
+		fg
+		echo;echo "Updating the server files"
+		git fetch --all
+		git reset --hard origin/master
 		sudo node app.js &
 		app=$!
-		sleep 3
 	else
-		echo "Up to date"
-		sleep 5
+		echo -n .
 	fi
+	sleep 5
 done
